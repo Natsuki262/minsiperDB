d57d8d87dd971021463b821fce70a76b3ff218df
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-05-28 02:00:00 +0900

    第一回配信終了ひとまずセルの表示と、地雷を埋めることができた

---
+++ New Unity Project (1)/Assets/Cell.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// 地雷をも持っているか　初期は持ってない
+    /// </summary>
+    bool isMineBuried = false;
+    /// <summary>
+    /// ゲットで地雷の情報を渡す
+    ///セッターで引数を代入
+    /// </summary>
+    public bool IsMineBuried
+    {
+
+        get
+        {
+            return isMineBuried;
+        }
+        set
+        {
+            isMineBuried = value;
+        }
+    }
+    /// <summary>
+    /// 爆弾の状態を示すテキスト
+    /// </summary>
+    [SerializeField]
+    Text mineStatusTxt;
+
+    void Start()
+    {
+        if (isMineBuried == true)
+        {
+            mineStatusTxt.text = "X";
+        }
+        else
+        {
+            mineStatusTxt.text = " ";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ New Unity Project (1)/Assets/GameManeger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManeger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// 行
+    /// </summary>
+    [SerializeField]
+    private int rows = 1;
+    /// <summary>
+    /// 列
+    /// </summary>
+    [SerializeField]
+    private int coulums = 1;
+
+    [SerializeField]
+    private GridLayoutGroup gridLayoutGroup = null;
+
+    [SerializeField]
+    private Cell Cellprefab = null;
+
+    /// <summary>
+    /// 行、列二次元配列
+    /// </summary>
+    Cell[,] array;
+    
+    void Start()
+    {
+        var parent = gridLayoutGroup.gameObject.transform;
+        if (coulums < rows)
+        {
+            gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            gridLayoutGroup.constraintCount = coulums;
+        }
+        else
+        {
+            gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+            gridLayoutGroup.constraintCount = rows;
+        }
+
+        for (var r = 0; r < rows; r++)
+        {
+            for (var c = 0; c < coulums; c++)
+            {
+
+                var cell = Instantiate(Cellprefab);
+                cell.IsMineBuried = true;
+                cell.transform.SetParent(parent);
+
+
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
