34edce306e3215747a312f9d1d0e52c8d5d3a16a
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-06-02 02:04:55 +0900

    第二回終了ランダムにボムを配置できたが、誤差が生じてしまうので次回直す

--- New Unity Project (1)/Assets/GameManeger.cs
+++ New Unity Project (1)/Assets/GameManeger.cs
@@ -21,15 +21,23 @@ public class GameManeger : MonoBehaviour
 
     [SerializeField]
     private Cell Cellprefab = null;
+    [SerializeField]
+    private int bombCount = 0;
+
+
 
     /// <summary>
     /// 行、列二次元配列
     /// </summary>
-    Cell[,] array;
-    
+    Cell[,] cellArray;
+
     void Start()
     {
+        //ここで配列の中にrowsとcoulumsのデータ配列を定義生成
+        cellArray = new Cell[rows, coulums];
+
         var parent = gridLayoutGroup.gameObject.transform;
+        //ぐりっとレイアウトグループのステータス設定
         if (coulums < rows)
         {
             gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
@@ -40,20 +48,50 @@ public class GameManeger : MonoBehaviour
             gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedRowCount;
             gridLayoutGroup.constraintCount = rows;
         }
-
+        //列形成
         for (var r = 0; r < rows; r++)
         {
             for (var c = 0; c < coulums; c++)
             {
 
-                var cell = Instantiate(Cellprefab);
-                cell.IsMineBuried = true;
+                Cell cell = Instantiate(Cellprefab);
+                //cellArray配列に要素番号をあてはめつつ、実態データを代入
+                cellArray[r, c] = cell;
+                //cell.IsMineBuried = true;
                 cell.transform.SetParent(parent);
 
+            }
 
+        }
+        //ランダムに爆弾を埋め込む
+        for (int i = 0; i < bombCount; i++)
+        {
+            int tmp = 0;
+            int tmp2 = 0;
+            for (int p = 0; p < int.MaxValue; p++)
+            {
+                tmp = Random.Range(0, rows);
+                tmp2 = Random.Range(0, coulums);
+                if (p < bombCount)
+                {
+                    break;
+                }
 
             }
+
+
+            if (cellArray[tmp, tmp2].IsMineBuried == true)
+            {
+
+                int tmp3 = Random.Range(0, rows);
+                int tmp4 = Random.Range(0, coulums);
+                cellArray[tmp3, tmp4].IsMineBuried = true;
+            }
+            cellArray[tmp, tmp2].IsMineBuried = true;
         }
+
+
+
     }
 
     // Update is called once per frame
